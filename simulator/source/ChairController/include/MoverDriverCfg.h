f497a5a132391e718aef19a16467ac249aafa46d
LSchoenbauer <l.schoenbauer@students.htl-leonding.ac.at>
2025-11-18 11:13:17 +0100

    added pin config header file for mover driver

---
+++ simulator/source/ChairController/include/MoverDriverCfg.h
@@ -0,0 +1,79 @@
+/*
+ * ===============================================================================
+ * c) HTL Leonding
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Licensed under MIT License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * All trademarks used in this document are property of their respective owners.
+ * ===============================================================================
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+/*
+    Pure configuration class for a single MoverDriver instance.
+
+    Contains only the hardware-specific configuration parameters:
+
+        - Step/dir pins
+        - Limit switches (top, center, bottom)
+        - Ramping steps
+        - Microstepping factor
+        
+    This class is a dumb data capsule, just like CommandData.
+*/
+
+class MoverDriverCfg {
+public:
+
+    MoverDriverCfg(
+        uint8_t pulsePin,
+        uint8_t dirPin,
+        uint8_t topSwitchPin,
+        uint8_t centerSwitchPin,
+        uint8_t bottomSwitchPin,
+        uint32_t rampingSteps = 50,
+        uint32_t microstepFactor = 1
+    ) :
+        mPulsePin(pulsePin),
+        mDirPin(dirPin),
+        mTopSwitchPin(topSwitchPin),
+        mCenterSwitchPin(centerSwitchPin),
+        mBottomSwitchPin(bottomSwitchPin),
+        mRampingSteps(rampingSteps),
+        mMicrostepFactor(microstepFactor)
+    {}
+
+    ~MoverDriverCfg() {}
+
+    // --- Getters -------------------------------------------------------------
+    uint8_t GetPulsePin()       const { return mPulsePin; }
+    uint8_t GetDirPin()         const { return mDirPin; }
+    uint8_t GetTopSwitchPin()   const { return mTopSwitchPin; }
+    uint8_t GetCenterSwitchPin()const { return mCenterSwitchPin; }
+    uint8_t GetBottomSwitchPin()const { return mBottomSwitchPin; }
+
+    uint32_t GetRampingSteps()  const { return mRampingSteps; }
+    uint32_t GetMicrostepFactor() const { return mMicrostepFactor; }
+
+private:
+    // --- Pins ---
+    uint8_t mPulsePin;
+    uint8_t mDirPin;
+    uint8_t mTopSwitchPin;
+    uint8_t mCenterSwitchPin;
+    uint8_t mBottomSwitchPin;
+
+    // --- Additional tuning ---
+    uint32_t mRampingSteps;
+    uint32_t mMicrostepFactor;
+};
+
